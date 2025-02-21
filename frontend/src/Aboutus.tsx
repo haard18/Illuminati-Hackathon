d67046bf507e53f2295347e941da0252b8cd8415
DevPatils <dev.patil1006@gmail.com>
2025-02-21 11:25:27 +0530

    I apolog

---
+++ frontend/src/Aboutus.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Aboutus = () => {
+    return (
+        <>
+
+            <div>
+                <div>
+                    <div>
+                        <h1>Explore now & get amazing deals
+                            on your bookings! jkdgfo823hblfbvuqgbe;kcngqe cljb
+                            bquietfol wejlkhcpikleq2 bvjlcgweuohn;lfi10kmcnqhcilnqfiohlkw cuiwgqiofln.
+                            uoqwyerpiqbwguof djlchask cuogqiolk cvyiqgifklnq.</h1>
+                    </div>
+                </div>
+                <div>
+
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Aboutus
