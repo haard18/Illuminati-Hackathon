ce1f77be6d1e1c018f83e3b55880fe008b6c0ac0
DevPatils <dev.patil1006@gmail.com>
2025-02-21 14:22:37 +0530

    Add Aboutus component to Home page with styling

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,25 +1,27 @@
 import Navbar from "../components/Navbar";
-
 import Hero from "../components/Landingpage/Hero";
 import Landing2 from "../components/Landing2";
 import RollingGallery from "../RollingGallery";
 import EventCarousel from "../components/EventCarousel";
 // import Events from "../components/Event";
+import Aboutus from "../Aboutus";
 
 const Home = () => {
   return (
     <>
-      <div className="bg-black ">
+      <div className="bg-black">
         <Navbar />
       </div>
-      <div className="">
+      <div>
         <Hero />
       </div>
       <div className="bg-black h-[800px] pt-20 mt-[-18%]">
         <Landing2 />
-        {/* <Events /> */}
         <EventCarousel/>
         <RollingGallery/>
+        <div className="flex justify-center items-center bg-black">
+          <Aboutus />
+        </div>
       </div>
     </>
   );
